d93ef8d47a43222b90a536ba5a5822f97b9bc33a
Nick Carneiro <burthawk101@gmail.com>
2012-03-22 16:29:28 -0500

    connect to servers in order

---
+++ src/client/TestSuite.java
@@ -0,0 +1,22 @@
+package client;
+
+
+
+public class TestSuite {
+	
+	public static void main(String[] args){
+		
+		// run some tests and make sure the output is the same
+		correctnessTests();
+		
+	}
+
+	/*
+	 * Test basic correctness
+	 */
+	private static void correctnessTests(){
+		UDPClient.run();
+		
+	}
+	
+}
--- src/client/UDPClient.java
+++ src/client/UDPClient.java
@@ -1,44 +1,80 @@
 package client;
 import java.net.*;
+import java.util.ArrayList;
 import java.io.*;
 
 public class UDPClient {
-	private String hostname;
+	private ArrayList<ServerAddr> servers;
+	private ServerAddr current_server;
 	public Integer port;
 	public Integer receive_length = 1024;
 
 	public DatagramSocket datasocket;
 	public InetAddress ia;
 	public DatagramPacket sPacket, rPacket;
-	public UDPClient(String hostname, Integer port){
+
+	public UDPClient(){
 		System.out.println("Initializing UDP Client");
 
+		//import list of servers
+		servers = new ArrayList<ServerAddr>();
+		BufferedReader br;
 		try {
-			this.port = port;
-			this.ia = InetAddress.getByName(hostname);
-			this.datasocket = new DatagramSocket();
+			br = new BufferedReader(new FileReader("servers.txt"));
 
-		} catch (UnknownHostException e) {
-			System.err.println(e);
-		} catch (SocketException se) {
-			System.err.println(se);
+			String line;
+			//read in servers.txt and save in arraylist
+			while ((line = br.readLine()) != null) {
+				ServerAddr address = new ServerAddr();
+				address.parseAddr(line);
+				servers.add(address);
+			}
+			
+			
+		} catch (FileNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
+		
+		//now we have a list of servers. Try to connect to them sequentially.
+		for(ServerAddr addr : servers){
+			try {
+				//keep track of current server in case we need it later.
+				current_server = addr;
+				this.port = Integer.parseInt(addr.port);
+				this.ia = InetAddress.getByName(addr.hostname);
+				this.datasocket = new DatagramSocket();
+				//if we get here, we have a valid hostname
+				break;
+
+			} catch (UnknownHostException e) {
+				System.err.println("Could not connect to server " + addr.hostname + ":" + addr.port + e);
+			} catch (SocketException se) {
+				System.err.println(se);
+			}
+		}
+		
 	}
 
 
+
+
 	public static void run() {
-		UDPClient client = new UDPClient("localhost", 8080);
+		UDPClient client = new UDPClient();
 		String line;
 		try {
-		BufferedReader br = new BufferedReader(new FileReader("testfile.txt"));
+			BufferedReader br = new BufferedReader(new FileReader("testfile.txt"));
 
-		//read in testfile.txt and send the commands to the server.
+			//read in testfile.txt and send the commands to the server.
 			while ((line = br.readLine()) != null) {
 				byte[] command = line.getBytes();
 				System.out.println("Sending command: " + line);
 				client.sPacket = new DatagramPacket(command, command.length, client.ia, client.port);
 				client.datasocket.send(client.sPacket);
-				
+
 				//create space for response
 				byte[] rbuffer = new byte[client.receive_length];
 				client.rPacket = new DatagramPacket(rbuffer, rbuffer.length);
@@ -53,5 +89,30 @@ public class UDPClient {
 			e.printStackTrace();
 		} // end while
 	}  // end main
+
+	class ServerAddr{
+		public String hostname;
+		public String port;
+		ServerAddr(String hostname, String port){
+			this.hostname = hostname;
+			this.port = port;
+		}
+		
+		ServerAddr(){
+			
+		}
+
+		//parses string like "192.168.1.101:3033"
+		public void parseAddr(String addr){
+			String[] addrSplit = addr.split(":");
+			if(addrSplit.length != 2){
+				throw new IllegalArgumentException("Bad address format.");
+			} else {
+				this.hostname = addrSplit[0];
+				this.port = addrSplit[0];
+			}
+
+		}
+	}
 
 }
